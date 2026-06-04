875aafbb16eadeb309cc31386ce93610a4ce9c17
Wahiba <73328123+howa9999-coder@users.noreply.github.com>
2026-06-04 06:08:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* ===============================
    CACHE NAME (change version to force update)
    =============================== */
-const cacheName = 'almo3ine-v000-02';
+const cacheName = 'almo3ine-v000-04';
 
 /* ===============================
    FILES TO CACHE FOR OFFLINE USE
